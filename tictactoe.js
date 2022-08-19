b19282287dc8118ef63de42ee21096167e78c42a
Bilbagbobins <aaronxt@hotmail.com>
2022-08-19 12:33:38 +1200

    put player move function within factory

--- tictactoe.js
+++ tictactoe.js
@@ -1,15 +1,35 @@
-function playerMove() {
-    if (this.textContent == '') {
-        this.textContent = 'X';
-        Gameboard[this.id] = 'X';
-    } 
+// function playerMove() {
+//     if (this.textContent === '') {
+//         this.textContent = 'X';
+//         Gameboard[this.id] = 'X';
+//     } 
+// }
+
+const Gameplay = (id) => {
+    // which active player?
+    activePlayer.playerMove(id);
 }
 
+const Player = (icon) => {
 
-const Player = () => {
-    const playerToken = 'X';
-    return  playerToken;
+    const playerMove = (id) => {
+        const cell = document.getElementById(`${id}`);
+        if (cell.textContent === '') {
+            cell.textContent = icon;
+            Gameboard[id] = icon;
+            console.log(Gameboard);
+        }
+    }
+    
+    return  {
+        playerMove
+    }
 }
+const player1 = Player('X');
+const player2 = Player('O');
+const activePlayer = player1;
+// create a player factory inside player module that runs when page loads
+// player name, vs comp?, etc
 
 const Gameboard = (() => {
     const board = Array.apply(null, Array(9));
@@ -18,7 +38,7 @@ const Gameboard = (() => {
         const cell = document.createElement('div');
         cell.classList.add('cell');
         cell.setAttribute('id', i);
-        cell.addEventListener('click', playerMove);
+        cell.addEventListener('click', () => Gameplay(i));
         boardContainer.appendChild(cell);
     }   
     return board;
